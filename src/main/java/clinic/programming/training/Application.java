7b77ba15c7871d24a5d5470a72ef2eab10e9d42b
Eduard <anemadevelop@gmail.com>
2020-05-05 01:05:24 +0200

    Usig StringUtils and addig dependency on POM file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,25 @@
 package clinic.programming.training;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
-    }
+	}
+	
+	public int countWords(String words){
+		String[] separatedWord = StringUtils.split(words, ' ');
+		return (separatedWord == null) ? 0 : 0;
+	}
+	
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	//System.out.println(app.countWords("hola que tal"));
+	
     }
 }
